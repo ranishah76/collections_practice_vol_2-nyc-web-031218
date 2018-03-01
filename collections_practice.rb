ee0e89bdb319368591798064d1172e1421d40124
Rani Shah <rani76@gmail.com>
2018-03-01 12:02:51 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,18 +1,4 @@
 # your code goes here
-def organize_schools(schools)
-  organized_schools = {}
-  schools.each do |name, location_hash|
-    location = location_hash[:location]
-    if organized_schools[location]
-      organized_schools[location] << name
-    else
-      organized_schools[location] = []
-      organized_schools[location] << name
-    end
-  end
-  organized_schools
-end
-
 def begins_with_r(array)
   flag = true
   array.each do |element|
@@ -29,14 +15,6 @@ def contain_a(array)
   container
 end
 
-def remove_non_strings(array)
-  container = []
-  array.each do |element|
-    container << element if element.is_a?(String)
-  end
-  container
-end
-
 def first_wa(array)
   first_wa = nil
   array.each do |element|
@@ -48,13 +26,14 @@ def first_wa(array)
   first_wa
 end
 
-def find_cool(array)
+def remove_non_strings(array)
   container = []
   array.each do |element|
-    container << element if element[:temperature] == "cool"
+    container << element if element.is_a?(String)
   end
   container
 end
+
 
 def count_elements(array)
   array.each do |original_hash|
@@ -81,4 +60,26 @@ def merge_data(keys, values)
     end
   end
   container
+end
+
+def find_cool(array)
+  container = []
+  array.each do |element|
+    container << element if element[:temperature] == "cool"
+  end
+  container
+end
+
+def organize_schools(schools)
+  organized_schools = {}
+  schools.each do |name, location_hash|
+    location = location_hash[:location]
+    if organized_schools[location]
+      organized_schools[location] << name
+    else
+      organized_schools[location] = []
+      organized_schools[location] << name
+    end
+  end
+  organized_schools
 end
